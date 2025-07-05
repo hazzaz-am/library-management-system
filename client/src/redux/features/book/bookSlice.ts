b9aa10d3f6af6e5878a4806de054b9990fb5844e
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-07-05 21:32:14 +0600

    feat: update url

--- client/src/redux/features/book/bookSlice.ts
+++ client/src/redux/features/book/bookSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const bookApi = createApi({
 	reducerPath: "bookApi",
-	baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/api" }),
+	baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_SERVER_URL }),
 	tagTypes: ["books", "borrows"],
 	endpoints: (build) => ({
 		getBooks: build.query({
